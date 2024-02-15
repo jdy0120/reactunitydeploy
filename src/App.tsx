a1fdd75451e939ea1f2efc01c0d7977b775b378b
Doherty Cho <jdy0120120@gmail.com>
2024-02-15 13:00:55 +0900

    update loading spin in bowser

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
+import { useEffect } from "react";
 import "./App.css";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: "Build/20240215.loader.js",
     dataUrl: "Build/20240215.data.unityweb",
     frameworkUrl: "Build/20240215.framework.js.unityweb",
     codeUrl: "Build/20240215.wasm.unityweb",
   });
 
+  useEffect(() => {
+    console.log(isLoaded);
+  }, [isLoaded]);
+
   return (
     <>
-      <h1
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          color: "black",
-          zIndex: 100,
-          opacity: 0.5,
-        }}
-      >
-        Test Unity WebGL
-      </h1>
+      {isLoaded === false && (
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
       <Unity
         unityProvider={unityProvider}
         style={{
